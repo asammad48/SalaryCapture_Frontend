a7bdd9be5d055fb436194c990488ce639e27c92d
Abdul Sammad <asammad48@gmail.com>
2026-01-28 07:54:31 +0000

    Removed extra salary files

--- core-ui-admin-library/src/lib/presentation/settings/settings.routes.ts
+++ core-ui-admin-library/src/lib/presentation/settings/settings.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 
-export const SETTINGS_ROUTES: Routes = [];
+export const SETTINGS_ROUTES: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./syncing/syncing.component').then((d) => d.SyncingComponent),
+    data: { title: 'Settings', breadcrumb: 'Settings' },
+  },
+];
